24043811daebd7f9150d61ac5bebf8f7ed16d5a6
Ramesh Marandi <rameshmarandi@Rameshs-MacBook-Air.local>
2025-04-12 22:57:38 +0530

    ios splash screen code is compelted

---
+++ src/utility/theme/font.ts
@@ -0,0 +1,16 @@
+export const fonts = {
+    en: {
+        regular: 'Poppins-Regular',
+        semiBold: 'Poppins-SemiBold',
+        medium: 'Poppins-Medium',
+        bold: 'Poppins-Bold',
+        extraBold: 'Poppins-ExtraBold',
+        italic: 'Poppins-Italic',
+        thin: 'Poppins-Thin',
+    },
+    hi: {
+      regular: 'Hindi-Regular',
+      bold: 'Hindi-Bold',
+    },
+  };
+  
